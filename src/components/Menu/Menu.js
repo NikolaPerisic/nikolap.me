77aefa681685579d2ecc6aa09e7c5d573876afca
Nikola Perisic <cathode.y2k@gmail.com>
2019-05-25 22:07:13 +0200

    menu component

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -9,33 +9,80 @@ const Menu = () => {
   };
   return (
     <header>
-      <div className={classes.menu__btn} onClick={toggleMenu}>
+      <div
+        className={
+          menu.showMenu
+            ? `${classes.menu__btn} ${classes.close}`
+            : classes.menu__btn
+        }
+        onClick={toggleMenu}
+      >
         <div className={classes.btn__line} />
         <div className={classes.btn__line} />
         <div className={classes.btn__line} />
       </div>
 
-      <nav className={classes.menu}>
-        <div className={classes.menu__branding}>
+      <nav
+        className={
+          !menu.showMenu ? classes.menu : `${classes.menu} ${classes.show}`
+        }
+      >
+        <div
+          className={
+            !menu.showMenu
+              ? classes.menu__branding
+              : `${classes.menu__branding} ${classes.show}`
+          }
+        >
           <div className={classes.portrait} />
         </div>
-        <ul className={classes.menu__nav}>
-          <li className={classes.nav__item}>
+        <ul
+          className={
+            !menu.showMenu
+              ? classes.menu__nav
+              : `${classes.menu__nav} ${classes.show}`
+          }
+        >
+          <li
+            className={
+              !menu.showMenu
+                ? classes.nav__item
+                : `${classes.nav__item} ${classes.show}`
+            }
+          >
             <a href="index.html" className={classes.nav__link}>
               Home
             </a>
           </li>
-          <li className={classes.nav__item}>
+          <li
+            className={
+              !menu.showMenu
+                ? classes.nav__item
+                : `${classes.nav__item} ${classes.show}`
+            }
+          >
             <a href="about.html" className={classes.nav__link}>
               About Me
             </a>
           </li>
-          <li className={classes.nav__item}>
+          <li
+            className={
+              !menu.showMenu
+                ? classes.nav__item
+                : `${classes.nav__item} ${classes.show}`
+            }
+          >
             <a href="work.html" className={classes.nav__link}>
               My Work
             </a>
           </li>
-          <li className={classes.nav__item}>
+          <li
+            className={
+              !menu.showMenu
+                ? classes.nav__item
+                : `${classes.nav__item} ${classes.show}`
+            }
+          >
             <a href="contact.html" className={classes.nav__link}>
               Contact Me
             </a>
